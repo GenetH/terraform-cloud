d43028f827fa3dc35a141141ab0fdc2a574f54ff
genethagos <genethagos@gmail.com>
2025-02-20 12:46:58 +0300

    variable edited

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,14 @@
-#var
+#variables
+variable "AWS_ACCESS_KEY_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+}
+
 variable "region" {
   type = string
   description = "The region to deploy resource"
