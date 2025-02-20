8eab348a40550c820009b6500413e799e0565cb3
genethagos <genethagos@gmail.com>
2025-02-20 12:11:42 +0300

    variable edited

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,13 @@
 #variables
+variable "AWS_ACCESS_KEY_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+}
 
 variable "region" {
   type = string
